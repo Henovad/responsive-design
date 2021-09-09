fc335162d2cefd71242bfdc35669bb62f12e9b39
Henovad <88333431+Henovad@users.noreply.github.com>
2021-09-09 14:45:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mainContentEl = document.getElementById("Main-Content")
 
 registerFormEl.addEventListener("submit", event => {
     event.preventDefault()
-
+    
     let ourFormData = new FormData(event.target)
 
     let firstName = ourFormData.get("firstName")
@@ -15,7 +15,7 @@ registerFormEl.addEventListener("submit", event => {
             <p>You're on your way to becoming a DESIGN Master!</p>
 
             <p class="fine-print">You will get weekly DESIGN tips sent to: ${emailAddress}</p>
-            <form id="Register-Form" class="register-form" action="#" method="POST">
+            <form id="Register-Form" class="register-form" action="" method="POST">
                 <button class="btn btn-primary" type="submit">Back</button>
             </form>`
     
